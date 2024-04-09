9870b7c9d54cdff626d93fdfda5156a4fd48caf1
priyanshik0902 <latakotian09@gmail.com>
2024-04-09 11:36:24 +0530

    6 commit

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/s30project/target
 sudo java -jar *.jar > /dev/null 2> /dev/null < /dev/null &
-cd /home/ec2-user/server/fitnessapp
+cd /home/ec2-user/server/fitnessapp/build
 sudo npm start
