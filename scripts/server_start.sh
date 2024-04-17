4c3bc1a67c9c39d2c08af2a871003c869248bc7a
priyanshik0902 <latakotian09@gmail.com>
2024-04-17 18:54:44 +0530

    making changes in start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar s30project-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+sudo java -jar -Dserver.port=80 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
 # cd /home/ec2-user/server/fitnessapp/build
 # sudo npm start
